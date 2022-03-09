0db0da8095180125138bf7c3abc8902ccdf1df0a
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-03-09 11:07:12 +0300

    Update project descriptions

---
+++ README.md
@@ -0,0 +1,5 @@
+# InstantSport
+
+[Project Link]()
+
+InstantSport is the place for on-demand sports news, and we wanted to make sports news easy and fun even for casual sports lovers. All sports in one place.
--- test.js
+++
@@ -1,8 +0,0 @@
-const content = document.querySelectorAll(
-  ".gridPostView .uabb-blog-posts-description p"
-);
-content.forEach((str) => {
-  if (str.innerHTML.length > 200) {
-    str.innerHTML = str.innerHTML.substring(0, 200) + "...";
-  }
-});
